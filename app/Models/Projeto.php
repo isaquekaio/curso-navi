bcc7f0c8a286b719d390aa8dfc0e8269712a1dc9
Isaque Kaio <isaque.araujo@escolar.ifrn.edu.br>
2020-01-27 10:38:27 -0300

    Adicionando e utilizando o softDeletes

--- app/Models/Projeto.php
+++ app/Models/Projeto.php
@@ -4,9 +4,12 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
 use App\User;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Projeto extends Model
 {
+    use SoftDeletes;
+
     protected $fillable = ['titulo', 'gerente_id'];
     //protected $guarded = [];
 
