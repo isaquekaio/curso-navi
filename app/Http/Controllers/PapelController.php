f2f48526dc6b4eb9167256062fcee2b5c32cc5a8
Isaque Kaio <isaque.araujo@escolar.ifrn.edu.br>
2020-02-03 10:27:00 -0300

    Implementando o CRUD refente a entidade Papel

---
+++ app/Http/Controllers/PapelController.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Papel;
+use Illuminate\Http\Request;
+
+class PapelController extends Controller
+{
+    public function index()
+    {
+        return response()->json(Papel::all());
+    }
+
+    public function show($id)
+    {
+        $papel = Papel::findOrFails($id);
+        return response()->json($papel);
+    }
+
+    public function store(Request $request)
+    {
+        $data = $this->validate($request, [
+            'papel' => 'request'
+        ]);
+
+        $papel = Papel::create($data);
+        return response()->json($papel);
+    }
+
+    public function update(Request $request, $id)
+    {
+        $data = $this->validate($request, [
+            'papel' => 'request'
+        ]);
+        $papel = Papel::findOrFail($id);
+        $papel->update($data);
+        return response()->json($papel);
+    }
+
+    public function destroy(Papel $papel)
+    {
+        //
+    }
+}
---
+++ app/Models/Papel.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Papel extends Model
+{
+    protected $fillable = ['papel'];
+
+    protected $table = 'papeis';
+}
